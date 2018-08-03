eeef5151527ada80d5c0f5e2377d1219b6880d12
eimask <minh.excellent@gmail.com>
2018-08-03 16:58:26 +0700

    change teams app

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    code = models.CharField(max_length=24)
+    name = models.CharField(max_length=192)
+    is_active = models.BooleanField(default=True)
