d9fd6de395ded4fcff484555dc3f6884bc2e6672
MinhPT <phamtuanminh1993@gmail.com>
2018-08-05 21:43:26 +0700

    add home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_view(*args, **kwargs):
+    return HttpResponse("<h1>This is homepage</h1>")
+
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>This is contact</h1>")
--- teams/models.py
+++ teams/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class Team(models.Model):
     code = models.CharField(max_length=24)
     name = models.CharField(max_length=192)
-    is_active = models.BooleanField(default=True)
+    isactive = models.BooleanField(default=True)
